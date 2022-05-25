b3583d51873fd126d55ce6532ad23e789fc50a0e
Joe <jiangchaochao1994@outlook.com>
2022-05-25 14:28:48 +0800

    init poroject

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+/*
+ * @Description:
+ * @file name: File name
+ * @version:
+ * @Author: Joe
+ * @Date: 2022-05-23 15:46:44
+ * @LastEditors: Joe
+ * @LastEditTime: 2022-05-25 13:32:18
+ */
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
